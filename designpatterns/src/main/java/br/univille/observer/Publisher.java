2f8e6f6ee333b74f14b343256830386c70c77064
RHUAN JOSE VOLTOLINI <rhuanvoltolini@univille.br>
2025-08-25 19:02:38 -0300

    publisher.java

--- designpatterns/src/main/java/br/univille/observer/Publisher.java
+++ designpatterns/src/main/java/br/univille/observer/Publisher.java
@@ -1,5 +1,69 @@
 package br.univille.observer;
 
-public class Publisher {
-    
+import java.util.ArrayList;
+import java.util.List;
+
+//A interface do Publicador, que é o objeto a ser "observado".
+
+interface Publisher {
+    void subscribe(Subscriber subscriber);
+    void unsubscribe(Subscriber subscriber);
+    void notifySubscribers();
 }
+
+
+//A interface do Assinante, que define o método de atualização.
+
+interface Subscriber {
+    void update(ConcretePublisher publisher);
+}
+
+
+//Publicador concreto que implementa a interface do Publicador.
+
+class ConcretePublisher implements Publisher {
+    private int state = 0;
+    private final List<Subscriber> subscribers = new ArrayList<>();
+
+    @Override
+    public void subscribe(Subscriber subscriber) {
+        System.out.println("Publisher: Adicionando assinante...");
+        subscribers.add(subscriber);
+    }
+
+    @Override
+    public void unsubscribe(Subscriber subscriber) {
+        System.out.println("Publisher: Removendo assinante...");
+        subscribers.remove(subscriber);
+    }
+
+    @Override
+    public void notifySubscribers() {
+        System.out.println("Publisher: Notificando assinantes...");
+        for (Subscriber subscriber : subscribers) {
+            subscriber.update(this);
+        }
+    }
+
+    public void someBusinessLogic() {
+        System.out.println("\nPublisher: Estou fazendo algo importante.");
+        this.state = this.state + 1;
+        System.out.println("Publisher: Meu estado mudou para: " + this.state);
+        notifySubscribers();
+    }
+
+    public int getState() {
+        return state;
+    }
+}
+
+
+//Um assinante concreto que implementa a interface do Assinante.
+
+class ConcreteSubscriber implements Subscriber {
+    @Override
+    public void update(ConcretePublisher publisher) {
+        System.out.println("Assinante: Recebi uma atualização. O estado do publicador é " + publisher.getState());
+    }
+}
+
