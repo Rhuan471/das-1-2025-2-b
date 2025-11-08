40d82d323dd085ea210c71d2f1adf7a6e500452d
Rhuan471 <rhuanjosevoltolini@gmail.com>
2025-11-08 14:37:35 -0300

    DAS

--- microkernel-plugin/service/ServiceOneImpl.java
+++ microkernel-plugin/service/ServiceOneImpl.java
@@ -3,9 +3,11 @@ package br.univille.microkernel_plugin.service;
 import java.util.HashMap;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
 
 import br.univille.microkernel_interface.service.DefaultService;
 
+@Service
 public class ServiceOneImpl implements DefaultService{
 
     @Override
@@ -13,5 +15,4 @@ public class ServiceOneImpl implements DefaultService{
         System.out.println("Service one" + params);
         return HttpStatus.OK;
     }
-
 }
--- microkernel-plugin/service/ServiceTwoImpl.java
+++ microkernel-plugin/service/ServiceTwoImpl.java
@@ -3,15 +3,16 @@ package br.univille.microkernel_plugin.service;
 import java.util.HashMap;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
 
 import br.univille.microkernel_interface.service.DefaultService;
 
-public class ServiceOneImpl implements DefaultService{
+@Service
+public class ServiceTwoImpl implements DefaultService{
 
     @Override
     public HttpStatus doWork(HashMap<String, String> params) {
         System.out.println("Service Two" + params);
         return HttpStatus.OK;
     }
-
 }
